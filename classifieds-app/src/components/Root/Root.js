ae6315fcc608613abc63eef134db7d06c46f1951
akifiak <akifiak@360insights.com>
2020-06-08 15:54:36 +0300

    7-step

--- classifieds-app/src/components/Root/Root.js
+++ classifieds-app/src/components/Root/Root.js
@@ -1,7 +1,12 @@
-import React from "react";
+import gql from "graphql-tag";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
 
-import Login from "./Login";
+import graphqlClient from "#root/api/graphqlClient";
+
+import AccountDetails from "./AccountDetails";
+import { setSession } from "#root/store/ducks/session";
 
 const Container = styled.div`
   display: flex;
@@ -27,12 +32,39 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
+const query = gql`
+    {
+        userSession(me: true) {
+            id
+            user {
+                email
+                id
+            }
+        }
+    }
+`;
+
 const Root = () => {
+  const dispatch = useDispatch();
+  const [initialised, setInitialised] = useState(false);
+
+  useEffect(() => {
+    graphqlClient.query({query}).then(({ data }) => {
+      if (data.userSession) {
+        dispatch(setSession(data.userSession));
+      }
+      setInitialised(true);
+    })
+  }, []);
+
+  if (!initialised) return "Loading...";
   return (
     <Wrapper>
       <Container>
         <Content>Content</Content>
-        <Login />
+        <Sidebar>
+          <AccountDetails />
+        </Sidebar>
       </Container>
     </Wrapper>
   )
